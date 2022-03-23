43e7af51117de16cd65b5852489f34217b181256
najib <raismnajib@com>
2022-03-23 11:47:51 +0700

    add public init so it can be used by everyone

--- Sources/SwiftUIView.swift
+++ Sources/SwiftUIView.swift
@@ -10,8 +10,9 @@ import SwiftUI
 
 //ADD TO GIT
 @available(iOS 13.0, *)
-struct SwiftUIView: View {
-    var body: some View {
+public struct SwiftUIView: View {
+    public init(){}
+    public var body: some View {
         Text("THIS IS THE SWIFTUIVIEW FROM THE PACKAGE")
     }
 }
